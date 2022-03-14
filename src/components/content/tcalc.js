2542f82f179a3474cc5fed67a35973338d160c03
PRAVEEN KUMAR <calltopraveen@gmail.com>
2022-03-14 18:37:03 +0530

    updates

--- src/components/content/tcalc.js
+++ src/components/content/tcalc.js
@@ -9,7 +9,7 @@ const Mortgage = () => {
     })
     const onSent = () => {
 
-        window.open(`https://morgage.nosnownaples.com/calc.html?LOANAMOUNT=${tcal.homePrice}&DOWNPAYMENT=${tcal.downPayment}+&DOWNPAYMENT_TYPE=Percent&TERM=${tcal.loadTerm}&TERM_UNITS=Years&INTERESTRATE=${tcal.interestRate}+&SHOWAMORTIZATIONSCHEDULE=on&COMPUTE=COMPUTE&CALCULATORID=HF02&TEMPLATE_ID=WWW.NOSNOWNAPLES.COM_1`)
+        window.open(`https://morgage.nosnownaples.com?LOANAMOUNT=${tcal.homePrice}&DOWNPAYMENT=${tcal.downPayment}+&DOWNPAYMENT_TYPE=Percent&TERM=${tcal.loadTerm}&TERM_UNITS=Years&INTERESTRATE=${tcal.interestRate}+&SHOWAMORTIZATIONSCHEDULE=on&COMPUTE=COMPUTE&CALCULATORID=HF02&TEMPLATE_ID=WWW.NOSNOWNAPLES.COM_1`)
         
     }
 
